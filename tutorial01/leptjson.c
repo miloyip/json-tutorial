d65896edce3cf786bd3e1cc68deb25adf6e7009b
byMyWay <orglanss@gmail.com>
2016-10-16 12:40:01 +0000

    finished practice

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -15,18 +15,42 @@ static void lept_parse_whitespace(lept_context* c) {
     c->json = p;
 }
 
+static int lept_parse_root_not_singular(lept_context* c){
+	lept_parse_whitespace(c);
+	return (c->json[0] == '\0')?LEPT_PARSE_OK:LEPT_PARSE_ROOT_NOT_SINGULAR;
+}
 static int lept_parse_null(lept_context* c, lept_value* v) {
     EXPECT(c, 'n');
     if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
         return LEPT_PARSE_INVALID_VALUE;
     c->json += 3;
     v->type = LEPT_NULL;
-    return LEPT_PARSE_OK;
+    return lept_parse_root_not_singular(c);
 }
+static int lept_parse_true(lept_context* c, lept_value* v) {
+    EXPECT(c, 't');
+    if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 3;
+    v->type = LEPT_TRUE;
+    return lept_parse_root_not_singular(c);
+}
+static int lept_parse_false(lept_context* c, lept_value* v) {
+    EXPECT(c, 'f');
+    if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 4;
+    v->type = LEPT_FALSE;
+    return lept_parse_root_not_singular(c);
+}
+
+
 
 static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
         case 'n':  return lept_parse_null(c, v);
+	case 't':  return lept_parse_true(c, v);
+	case 'f':  return lept_parse_false(c, v);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
         default:   return LEPT_PARSE_INVALID_VALUE;
     }
