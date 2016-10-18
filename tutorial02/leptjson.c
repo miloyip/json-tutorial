8867ef8d2bffbb4ec6e44c1884c57436fb1b1672
byMyWay <orglanss@gmail.com>
2016-10-18 05:36:57 +0000

    finished practise

--- tutorial02/leptjson.c
+++ tutorial02/leptjson.c
@@ -1,9 +1,10 @@
 #include "leptjson.h"
 #include <assert.h>  /* assert() */
 #include <stdlib.h>  /* NULL, strtod() */
-
+#include <math.h>
 #define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
-
+#define ISDIGIT(ch) ((ch) >= '0'&& (ch) <= '9')
+#define ISDIGIT1TO9(ch) ((ch) >= '1' && (ch) <= '9')
 typedef struct {
     const char* json;
 }lept_context;
@@ -63,14 +64,47 @@ static int lept_parse_literal(lept_context* c, lept_value* v){
 	return LEPT_PARSE_INVALID_VALUE;
 	
 }
+/*validate number and return where the number end*/
+static int valid_number(const char* c){
+	int i = 0;
+	if(c[i] == '-'){
+		i++;	
+	}
+	if(ISDIGIT(c[i])){
+		if(ISDIGIT1TO9(c[i])){
+			i++;
+			while(ISDIGIT(c[i]))i++;
+		}else i++;
+		if(c[i] == '.'){
+			i++;
+			if(ISDIGIT(c[i]))
+				while(ISDIGIT(c[i]))i++;
+			else
+				return 0;
+		}
+		if(c[i] == 'e' || c[i] == 'E'){
+			i++;
+			if(c[i] == '+' || c[i] == '-')i++;
+			if(ISDIGIT(c[i]))
+				while(ISDIGIT(c[i]))i++;
+			else
+				return 0;
 
+		}
+		return i;
+	}
+	return 0;
+}
 static int lept_parse_number(lept_context* c, lept_value* v) {
-    char* end;
-    /* \TODO validate number */
-    v->n = strtod(c->json, &end);
-    if (c->json == end)
-        return LEPT_PARSE_INVALID_VALUE;
-    c->json = end;
+    int end;
+    if((end = valid_number(c->json)) == 0)
+	    return LEPT_PARSE_INVALID_VALUE;
+    v->n = strtod(c->json, NULL);
+    if(v->n == HUGE_VAL || v->n == -HUGE_VAL){
+	    v->type = LEPT_NULL;
+	    return LEPT_PARSE_NUMBER_TOO_BIG;
+    }
+    c->json += end;
     v->type = LEPT_NUMBER;
     return LEPT_PARSE_OK;
 }
@@ -82,8 +116,7 @@ static int lept_parse_value(lept_context* c, lept_value* v) {
         case 'n':  return lept_parse_literal(c, v);
         default:   return lept_parse_number(c, v);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
-    }
-}
+    } }
 
 int lept_parse(lept_value* v, const char* json) {
     lept_context c;
