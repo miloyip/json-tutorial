0325c34000ced8e2b642b1024ad6b65218b3bb2d
AlexTuan1024 <alextuan1024@outlook.com>
2016-11-05 01:13:57 +0800

    my version with comments

--- tutorial01/leptjson.h
+++ tutorial01/leptjson.h
@@ -1,21 +1,40 @@
 #ifndef LEPTJSON_H__
 #define LEPTJSON_H__
 
-typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
+/*
+json中有6种数据类型，把true和false当成两种，共7种，用枚举表示，并定义为一个类型
+*/
+typedef enum {
+  LEPT_NULL,
+  LEPT_FALSE,
+  LEPT_TRUE,
+  LEPT_NUMBER,
+  LEPT_STRING,
+  LEPT_ARRAY
+} lept_type;
 
-typedef struct {
-    lept_type type;
-}lept_value;
+/*
+声明json的数据结构，每个节点使用lept_value来表示
+*/
+typedef struct { lept_type type; } lept_value;
 
+/*
+lept_parse的返回值类型
+*/
 enum {
-    LEPT_PARSE_OK = 0,
-    LEPT_PARSE_EXPECT_VALUE,
-    LEPT_PARSE_INVALID_VALUE,
-    LEPT_PARSE_ROOT_NOT_SINGULAR
+  LEPT_PARSE_OK = 0,
+  LEPT_PARSE_EXPECT_VALUE,
+  LEPT_PARSE_INVALID_VALUE,
+  LEPT_PARSE_ROOT_NOT_SINGULAR
 };
 
-int lept_parse(lept_value* v, const char* json);
+/*
+解析json函数
+*/
+int lept_parse(lept_value *v, const char *json);
 
-lept_type lept_get_type(const lept_value* v);
-
-#endif /* LEPTJSON_H__ */
+/*
+访问结果的函数，返回数据类型
+*/
+lept_type lept_get_type(const lept_value *v);
+#endif // LEPTJSON_H__
