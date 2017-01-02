e925c8eb29dba5583ee51bdadfe36c31838fb9c8
xmfbit <xmfbit@gmail.com>
2017-01-02 12:07:46 +0800

    complete tutor01

--- tutorial01/leptjson.h
+++ tutorial01/leptjson.h
@@ -1,17 +1,19 @@
 #ifndef LEPTJSON_H__
 #define LEPTJSON_H__
 
+/* JSON types */
 typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
 
 typedef struct {
     lept_type type;
 }lept_value;
 
+/* parsing state */
 enum {
-    LEPT_PARSE_OK = 0,
-    LEPT_PARSE_EXPECT_VALUE,
-    LEPT_PARSE_INVALID_VALUE,
-    LEPT_PARSE_ROOT_NOT_SINGULAR
+    LEPT_PARSE_OK = 0,           
+    LEPT_PARSE_EXPECT_VALUE,      
+    LEPT_PARSE_INVALID_VALUE,     
+    LEPT_PARSE_ROOT_NOT_SINGULAR  
 };
 
 int lept_parse(lept_value* v, const char* json);
