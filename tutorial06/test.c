bcdeec877ee3a1dad8102b7acdc0e769db7aa891
xmfbit <xmfbit@gmail.com>
2017-01-06 15:13:52 +0800

    complete tutor6

--- tutorial06/test.c
+++ tutorial06/test.c
@@ -364,9 +364,9 @@ static void test_parse() {
     test_parse_invalid_unicode_hex();
     test_parse_invalid_unicode_surrogate();
     test_parse_miss_comma_or_square_bracket();
-    /*test_parse_miss_key();
+    test_parse_miss_key();
     test_parse_miss_colon();
-    test_parse_miss_comma_or_curly_bracket();*/
+    test_parse_miss_comma_or_curly_bracket();
 }
 
 static void test_access_null() {
