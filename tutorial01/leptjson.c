a2533c021afe1018d9501206a9da4a7e5acf9835
FerrisChan <ferris.chan@foxmail.com>
2019-08-13 00:02:10 +0800

    master, home work 0

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -33,12 +33,18 @@ static int lept_parse_value(lept_context* c, lept_value* v) {
 }
 
 int lept_parse(lept_value* v, const char* json) {
+    int ret;
     lept_context c;
     assert(v != NULL);
     c.json = json;
     v->type = LEPT_NULL;
     lept_parse_whitespace(&c);
-    return lept_parse_value(&c, v);
+    if ((ret =lept_parse_value(&c, v)) == LEPT_PARSE_OK) {
+        lept_parse_whitespace(&c);
+        if (*c.json != '\0')
+            ret = LEPT_PARSE_ROOT_NOT_SINGULAR;
+    }
+    return  ret;
 }
 
 lept_type lept_get_type(const lept_value* v) {
--- tutorial01/test.c
+++ tutorial01/test.c
@@ -22,8 +22,22 @@ static int test_pass = 0;
 
 static void test_parse_null() {
     lept_value v;
-    v.type = LEPT_FALSE;
+    v.type = LEPT_NULL;
     EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "null"));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+}
+
+static void test_parse_true() {
+    lept_value v;
+    v.type = LEPT_TRUE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "true"));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+}
+
+static void test_parse_false() {
+    lept_value v;
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "false"));
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
 
