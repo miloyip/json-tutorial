7f021feb14c3a1c9c4d208748c2ea71758fbbc14
MoriSummer <1136153224@qq.com>
2021-11-23 16:45:39 +0800

    chapter1: finished

--- tutorial01/test.c
+++ tutorial01/test.c
@@ -7,6 +7,7 @@ static int main_ret = 0;
 static int test_count = 0;
 static int test_pass = 0;
 
+//测试框架
 #define EXPECT_EQ_BASE(equality, expect, actual, format) \
     do {\
         test_count++;\
@@ -17,7 +18,7 @@ static int test_pass = 0;
             main_ret = 1;\
         }\
     } while(0)
-
+//测试函数宏
 #define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
 
 static void test_parse_null() {
@@ -58,11 +59,17 @@ static void test_parse_root_not_singular() {
 }
 
 static void test_parse_false(){
-
+    lept_value v;
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "false"));
+    EXPECT_EQ_INT(LEPT_FALSE, lept_get_type(&v));
 }
 
 static void test_parse_true(){
-
+    lept_value v;
+    v.type = LEPT_TRUE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "true"));
+    EXPECT_EQ_INT(LEPT_TRUE, lept_get_type(&v));
 }
 
 static void test_parse() {
@@ -70,6 +77,8 @@ static void test_parse() {
     test_parse_expect_value();
     test_parse_invalid_value();
     test_parse_root_not_singular();
+    test_parse_true();
+    test_parse_false();
 }
 
 int main() {
