f167da691916e87b1a8cf879ceb0768c1b0f1885
MoriSummer <1136153224@qq.com>
2021-11-23 17:06:48 +0800

    tutorial2 : (1) Refactor

--- tutorial01_answer/leptjson.c
+++ tutorial01_answer/leptjson.c
@@ -15,38 +15,32 @@ static void lept_parse_whitespace(lept_context* c) {
     c->json = p;
 }
 
-static int lept_parse_true(lept_context* c, lept_value* v) {
-    EXPECT(c, 't');
-    if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
-        return LEPT_PARSE_INVALID_VALUE;
-    c->json += 3;
-    v->type = LEPT_TRUE;
-    return LEPT_PARSE_OK;
-}
 
-static int lept_parse_false(lept_context* c, lept_value* v) {
-    EXPECT(c, 'f');
-    if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
-        return LEPT_PARSE_INVALID_VALUE;
-    c->json += 4;
-    v->type = LEPT_FALSE;
-    return LEPT_PARSE_OK;
-}
-
-static int lept_parse_null(lept_context* c, lept_value* v) {
-    EXPECT(c, 'n');
-    if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
-        return LEPT_PARSE_INVALID_VALUE;
+static int lept_parse_literal(lept_context * c, lept_value * v, char prefix){
+    c->json++;
+    switch(prefix){
+        case 'f': if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
+                return LEPT_PARSE_INVALID_VALUE;
+        case 't': if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
+                return LEPT_PARSE_INVALID_VALUE;
+        case 'n': if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
+                return LEPT_PARSE_INVALID_VALUE;
+    }
     c->json += 3;
-    v->type = LEPT_NULL;
+    if(prefix == 'f') c->json++;
+    switch (prefix) {
+        case 'f': v->type = LEPT_FALSE;
+        case 't': v->type = LEPT_TRUE;
+        case 'n': v->type = LEPT_NULL;
+    }
     return LEPT_PARSE_OK;
 }
 
 static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
-        case 't':  return lept_parse_true(c, v);
-        case 'f':  return lept_parse_false(c, v);
-        case 'n':  return lept_parse_null(c, v);
+        case 't':  return lept_parse_literal(c, v, 't');
+        case 'f':  return lept_parse_literal(c, v, 'f');
+        case 'n':  return lept_parse_literal(c, v, 'n');
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
         default:   return LEPT_PARSE_INVALID_VALUE;
     }
