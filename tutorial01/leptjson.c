675f52e8d51037ebdfc04d1abd5a73713847a766
DESKTOP-MQHVN04\wenji <wen.jia.zhou@163.com>
2023-11-13 14:29:50 +0800

    修改lept_parse，增加了后空白判断逻辑 增加了false 和 true的转换函数，并添加对应的测试代码

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -1,13 +1,18 @@
 #include "leptjson.h"
 #include <assert.h>  /* assert() */
 #include <stdlib.h>  /* NULL */
-
+/*
+ * 判断json是不是字符串
+ */
 #define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
 
 typedef struct {
     const char* json;
 }lept_context;
-
+/*
+ * 传入一个上下文，获取上下文的字符串，找到第一个非空白格的地方
+ * 功能：将上下文中的字符串指针指向第一个字符地址
+ */
 static void lept_parse_whitespace(lept_context* c) {
     const char *p = c->json;
     while (*p == ' ' || *p == '\t' || *p == '\n' || *p == '\r')
@@ -15,6 +20,9 @@ static void lept_parse_whitespace(lept_context* c) {
     c->json = p;
 }
 
+/*
+ * 功能：判断是不是null
+ */
 static int lept_parse_null(lept_context* c, lept_value* v) {
     EXPECT(c, 'n');
     if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
@@ -23,10 +31,33 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
     v->type = LEPT_NULL;
     return LEPT_PARSE_OK;
 }
-
+/*
+ * 功能：判断是不是false
+ */
+static int lept_parse_false(lept_context* c, lept_value* v) {
+    EXPECT(c, 'f');
+    if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's'|| c->json[3] != 'e')
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 4;
+    v->type = LEPT_FALSE;
+    return LEPT_PARSE_OK;
+}
+/*
+ * 功能：判断是不是true
+ */
+static int lept_parse_true(lept_context* c, lept_value* v) {
+    EXPECT(c, 't');
+    if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 3;
+    v->type = LEPT_TRUE;
+    return LEPT_PARSE_OK;
+}
 static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
         case 'n':  return lept_parse_null(c, v);
+        case 'f':  return lept_parse_false(c, v);
+        case 't':  return lept_parse_true(c, v);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
         default:   return LEPT_PARSE_INVALID_VALUE;
     }
@@ -34,11 +65,17 @@ static int lept_parse_value(lept_context* c, lept_value* v) {
 
 int lept_parse(lept_value* v, const char* json) {
     lept_context c;
+    int ret;
     assert(v != NULL);
     c.json = json;
     v->type = LEPT_NULL;
     lept_parse_whitespace(&c);
-    return lept_parse_value(&c, v);
+    if((ret=lept_parse_value(&c, v))==LEPT_PARSE_OK){
+        lept_parse_whitespace(&c);
+        if (*c.json != '\0')
+            ret = LEPT_PARSE_ROOT_NOT_SINGULAR;
+    }
+    return ret;
 }
 
 lept_type lept_get_type(const lept_value* v) {
--- tutorial01/test.c
+++ tutorial01/test.c
@@ -27,6 +27,19 @@ static void test_parse_null() {
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
 
+static void test_parse_false() {
+    lept_value v;
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "  false  "));
+    EXPECT_EQ_INT(LEPT_FALSE, lept_get_type(&v));
+}
+static void test_parse_true() {
+    lept_value v;
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "  true  "));
+    EXPECT_EQ_INT(LEPT_TRUE, lept_get_type(&v));
+}
+
 static void test_parse_expect_value() {
     lept_value v;
 
@@ -62,6 +75,8 @@ static void test_parse() {
     test_parse_expect_value();
     test_parse_invalid_value();
     test_parse_root_not_singular();
+    test_parse_false();
+    test_parse_true();
 }
 
 int main() {
