3bc95ec87dfa3f837863b1ce785cfff6dc58078f
Hyan1ce <Hyanice_2502@outlook.com>
2024-03-16 14:01:52 +0800

    new file:   .vscode/c_cpp_properties.json 	new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	modified:   tutorial01/leptjson.c 	modified:   tutorial01/leptjson.h

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -1,21 +1,29 @@
 #include "leptjson.h"
-#include <assert.h>  /* assert() */
-#include <stdlib.h>  /* NULL */
+#include <assert.h> /* assert() */
+#include <stdlib.h> /* NULL */
 
-#define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
+#define EXPECT(c, ch)             \
+    do                            \
+    {                             \
+        assert(*c->json == (ch)); \
+        c->json++;                \
+    } while (0)
 
-typedef struct {
-    const char* json;
-}lept_context;
+typedef struct
+{
+    const char *json;
+} lept_context;
 
-static void lept_parse_whitespace(lept_context* c) {
+static void lept_parse_whitespace(lept_context *c)
+{
     const char *p = c->json;
     while (*p == ' ' || *p == '\t' || *p == '\n' || *p == '\r')
         p++;
     c->json = p;
 }
 
-static int lept_parse_null(lept_context* c, lept_value* v) {
+static int lept_parse_null(lept_context *c, lept_value *v)
+{
     EXPECT(c, 'n');
     if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
         return LEPT_PARSE_INVALID_VALUE;
@@ -24,15 +32,21 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
     return LEPT_PARSE_OK;
 }
 
-static int lept_parse_value(lept_context* c, lept_value* v) {
-    switch (*c->json) {
-        case 'n':  return lept_parse_null(c, v);
-        case '\0': return LEPT_PARSE_EXPECT_VALUE;
-        default:   return LEPT_PARSE_INVALID_VALUE;
+static int lept_parse_value(lept_context *c, lept_value *v)
+{
+    switch (*c->json)
+    {
+    case 'n':
+        return lept_parse_null(c, v);
+    case '\0':
+        return LEPT_PARSE_EXPECT_VALUE;
+    default:
+        return LEPT_PARSE_INVALID_VALUE;
     }
 }
 
-int lept_parse(lept_value* v, const char* json) {
+int lept_parse(lept_value *v, const char *json)
+{
     lept_context c;
     assert(v != NULL);
     c.json = json;
@@ -41,7 +55,8 @@ int lept_parse(lept_value* v, const char* json) {
     return lept_parse_value(&c, v);
 }
 
-lept_type lept_get_type(const lept_value* v) {
+lept_type lept_get_type(const lept_value *v)
+{
     assert(v != NULL);
     return v->type;
 }
