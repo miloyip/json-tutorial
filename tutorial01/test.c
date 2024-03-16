3ecfca44e521cbb9631c721947715e2a8a39c4d5
Hyan1ce <Hyanice_2502@outlook.com>
2024-03-16 15:54:36 +0800

    new file:   .vscode/tasks.json 	modified:   tutorial01/leptjson.c 	modified:   tutorial01/test.c 	new file:   tutorial01/test.exe

--- tutorial01/test.c
+++ tutorial01/test.c
@@ -2,32 +2,53 @@
 #include <stdlib.h>
 #include <string.h>
 #include "leptjson.h"
+#include "leptjson.c"
 
 static int main_ret = 0;
 static int test_count = 0;
 static int test_pass = 0;
 
-#define EXPECT_EQ_BASE(equality, expect, actual, format) \
-    do {\
-        test_count++;\
-        if (equality)\
-            test_pass++;\
-        else {\
-            fprintf(stderr, "%s:%d: expect: " format " actual: " format "\n", __FILE__, __LINE__, expect, actual);\
-            main_ret = 1;\
-        }\
-    } while(0)
+#define EXPECT_EQ_BASE(equality, expect, actual, format)                                                           \
+    do                                                                                                             \
+    {                                                                                                              \
+        test_count++;                                                                                              \
+        if (equality)                                                                                              \
+            test_pass++;                                                                                           \
+        else                                                                                                       \
+        {                                                                                                          \
+            fprintf(stderr, "%s:%d: expect: " format " actual: " format "\n", __FILE__, __LINE__, expect, actual); \
+            main_ret = 1;                                                                                          \
+        }                                                                                                          \
+    } while (0)
 
 #define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
 
-static void test_parse_null() {
+static void test_parse_null()
+{
     lept_value v;
     v.type = LEPT_FALSE;
     EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "null"));
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
 
-static void test_parse_expect_value() {
+static void test_parse_true()
+{
+    lept_value v;
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "true"));
+    EXPECT_EQ_INT(LEPT_TRUE, lept_get_type(&v));
+}
+
+static void test_parse_false()
+{
+    lept_value v;
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "false"));
+    EXPECT_EQ_INT(LEPT_FALSE, lept_get_type(&v));
+}
+
+static void test_parse_expect_value()
+{
     lept_value v;
 
     v.type = LEPT_FALSE;
@@ -39,7 +60,8 @@ static void test_parse_expect_value() {
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
 
-static void test_parse_invalid_value() {
+static void test_parse_invalid_value()
+{
     lept_value v;
     v.type = LEPT_FALSE;
     EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&v, "nul"));
@@ -50,21 +72,26 @@ static void test_parse_invalid_value() {
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
 
-static void test_parse_root_not_singular() {
+static void test_parse_root_not_singular()
+{
     lept_value v;
     v.type = LEPT_FALSE;
     EXPECT_EQ_INT(LEPT_PARSE_ROOT_NOT_SINGULAR, lept_parse(&v, "null x"));
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
 
-static void test_parse() {
+static void test_parse()
+{
     test_parse_null();
+    test_parse_true();
+    test_parse_false();
     test_parse_expect_value();
     test_parse_invalid_value();
     test_parse_root_not_singular();
 }
 
-int main() {
+int main()
+{
     test_parse();
     printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
     return main_ret;
